6b751aabd63f9a44db6229856f9bcd9245291808
salimb-source <salimb-source@users.noreply.github.com>
2026-09-06 16:11:45 +0000

    Force le violet Let’s Play et un logo plus grand sur À propos

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,6 @@ import App from "./App";
 import "./index.css";
 import "./LightModeSections.css";
 import "./OpenSansTypography.css";
+import "./AboutLetsPlayViolet.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
